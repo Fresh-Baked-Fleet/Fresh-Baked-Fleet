4674c8e4991688811d9f8128d7aba158c44c2a90
murilolhernandes <murilo.luiz.hernandes@gmail.com>
2026-06-16 16:12:25 -0600

    Fixed the bug that wasn't letting the user update their account info.

--- BlazorApp/Models/User.cs
+++ BlazorApp/Models/User.cs
@@ -12,7 +12,6 @@ namespace BlazorApp.Models
   {
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
-    public string ResidentialAddress { get; set; } = string.Empty;
     public string StreetAddress { get; set; } = string.Empty;
     public string City { get; set; } = string.Empty;
     public string State { get; set; } = string.Empty;
