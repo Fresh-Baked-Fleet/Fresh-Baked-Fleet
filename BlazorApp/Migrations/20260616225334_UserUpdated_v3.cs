434bb47c06720bbfc376bcd241f2390696941f6f
murilolhernandes <murilo.luiz.hernandes@gmail.com>
2026-06-16 17:00:45 -0600

    Fixed the dollar sign, login issues, and added PageTitle attributes to the Order page and Create Order page.

---
+++ BlazorApp/Migrations/20260616225334_UserUpdated_v3.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserUpdated_v3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ResidentialAddress",
+                table: "AspNetUsers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ResidentialAddress",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
